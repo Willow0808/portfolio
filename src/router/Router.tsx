8735b3a0080a56970e02461817bcf81223b4d4cc
TimothyKim <demodigim0823@gmail.com>
2022-12-20 22:01:48 +0900

    eighth commit

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,15 +3,18 @@ import { Home } from "../components/pages/Home"
 import { About } from "../components/pages/About"
 import { Project } from "../components/pages/Project"
 
+
+const homeUrl = process.env.PUBLIC_URL;
+
 const Router = () => {
     return(
         <BrowserRouter>
             <Routes>
-                <Route path="home" element={<Home></Home>}>
+                <Route path={ homeUrl + "/home" } element={<Home></Home>}>
                 </Route>
-                <Route path="about" element={<About></About>}>
+                <Route path={ homeUrl + "/about" } element={<About></About>}>
                 </Route>
-                <Route path="project" element={<Project></Project>}>
+                <Route path={ homeUrl + "/project" } element={<Project></Project>}>
                 </Route>
             </Routes>
         </BrowserRouter>
